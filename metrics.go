68bd12ea7eb6b2078f3ca35dc1db12d6b6257d30
Pascal van Leeuwen <pascal@grove.city>
2025-09-11 21:29:06 +0100

    fix linter

--- metrics.go
+++ metrics.go
@@ -8,14 +8,6 @@ import (
 	"github.com/buildwithgrove/path-external-auth-server/metrics"
 )
 
-// TODO_TECHDEBT(@adshmh): Support configurable pprof server address/port.
-const (
-	// pprofAddr is the address at which pprof server will be listening.
-	// NOTE: This address was selected based on the example here:
-	// https://pkg.go.dev/net/http/pprof
-	pprofAddr = ":6060"
-)
-
 // setupMetricsServer initializes and starts the Prometheus metrics server at the supplied address.
 func setupMetricsServer(logger polylog.Logger, addr, version string) error {
 	return metrics.ServeMetrics(logger, addr, version)
